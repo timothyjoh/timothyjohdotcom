fe8a36279cb3fd37b806fa62603a2965220fc458
Timothy Johnson <jhnsntmthy@gmail.com>
2025-02-09 17:03:05 -0500

    formbrick?

---
+++ src/components/Formbrick.tsx
@@ -0,0 +1,16 @@
+
+  // other imports
+  import formbricks from "@formbricks/js";
+  
+  if (typeof window !== "undefined") {
+    formbricks.init({
+      environmentId: "cm6y63ase0008snv6m79454ar",
+      apiHost: "https://f.timothyjoh.com",
+    });
+  }
+  
+  export const Formbrick = () => {
+    // your own Formbrick
+  }
+    
+  
