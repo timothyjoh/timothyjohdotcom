b983825b0bb4a98be369abf15394cd9937977f91
Timothy Johnson <jhnsntmthy@gmail.com>
2025-03-12 08:35:55 -0400

    added splash ?

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,22 @@ import tailwind from '@astrojs/tailwind'
 
 import vercel from '@astrojs/vercel';
 
+import sentry from '@sentry/astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  }), sentry({
+    dsn: "https://f5000fbbfc28bf36f33b9dffd3aec56f@o4506564404903936.ingest.us.sentry.io/4508964773756928",
+    tracesSampleRate: 0,
+    replaysSessionSampleRate: 0,
+    replaysOnErrorSampleRate: 0,
+    sourceMapsUploadOptions: {
+      project: "timothyjohdotcom",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    },
+  })],
 
   adapter: vercel(),
 })
